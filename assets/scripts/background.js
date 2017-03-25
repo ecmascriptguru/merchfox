26312e76f210c4b3e13c0458be74ecbde0d1769c
ecmascriptguru <ecmascript.guru@gmail.com>
2017-03-25 02:24:51 -0400

    content script basic workflow created.

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -10,19 +10,83 @@ var WordFox = (function() {
 	var _storage = window.DataStorage,
 		_amazonStartPoint = "https://www.amazon.com/s/ref=amb_link_483004722_1?ie=UTF8&field-keywords=&bbn=12035955011&field-enc-merchantbin=ATVPDKIKX0DER&hidden-keywords=ORCA&rh=i%3Afashion-novelty",
 		_urls = [],
+		_results = [],
 		_status = {
 			_started: false,
+			_detailPageStarted: false,
 			_initTabId: null,
 			_initTabUrl: null,
 			_detailTabIds: [],
-			_urlsCount: 1340,
-			_detailsCount: 240
+			_detailTabUrls: {},
+			_urlsCount: 0,
+			_detailsCount: 0
 		},
+
+		addUrls = function(params) {
+			_urls = _urls.concat(params.urls);
+			if (params.next_page_url || _urls.length < 10) {
+				_status._initTabUrl = params.next_page_url;
+				_status._urlsCount++;
+			} else {
+				if (!_status._detailPageStarted) {
+					startProductPages();
+				}
+				chrome.tabs.remove(_status._initTabId, function(res) {
+					console.log(param);
+					_status._initTabId = null;
+					_status._initTabUrl = null;
+				});
+			}
+		},
+
+		addData = function(data, sender, callback) {
+			_results.push(data);
+			_status._detailsCount++;
+
+			var nextPageUrl = _urls.pop();
+			if (nextPageUrl) {
+				_status._detailTabUrls[sender.tab.id] = nextPageUrl;
+				sendResponse({continue: true, next_url: nextPageUrl});
+			} else {
+				delete _status._detailTabUrls[sender.tab.id];
+				ind = _status._detailTabIds.indexOf(sender.tab.id);
+				_status._detailTabIds = _status._detailTabIds.slice(0, ind - 1).concat(_status._detailTabIds.slice(ind + 1));
+				sendResponse({continue: false});
+				chrome.tabs.remove(sender.tab.id);
+			}
+		},
+
+		initStatus = function() {
+			_status = {
+				_started: false,
+				_detailPageStarted: false,
+				_initTabId: null,
+				_initTabUrl: null,
+				_detailTabIds: [],
+				_detailTabUrls: {},
+				_urlsCount: 0,
+				_detailsCount: 0,
+				_results: []
+			};
+		},
+
+		startProductPages = function() {
+			_status._detailPageStarted = true;
+			for (var i = 0; i < Math.min(5, _urls.length); i++) {
+				var curDetailPageUrl = _urls.pop();
+				createTab({url: curDetailPageUrl}, function(tab) {
+					_status._detailTabIds.push(tab.id);
+					_status._detailTabUrls[tab.id] = curDetailPageUrl;
+				});
+			}
+		},
+
 		startedHandler = function(tab) {
-			//
+			initStatus();
 			console.log("Start url created. The tab info is ", tab.id);
 			_status._started = true;
 			_status._initTabId = tab.id;
+			_status._initTabUrl = tab.url;
 			chrome.browserAction.setBadgeText({ text: 'wait' });
 		},
 		start = function() {
@@ -35,18 +99,21 @@ var WordFox = (function() {
 				_status._started = false;
 				_status._initTabId = null;
 				_status._initTabUrl = null;
+				_status._detailPageStarted = false;
 				chrome.browserAction.setBadgeText({ text:'' });
 			});
 		},
-		getSomething = function() {
-			return [];
+		getUrls = function() {
+			return _urls;
 		};
 
 	return {
 		status: _status,
 		start: start,
 		stop: stop,
-		get: getSomething()
+		addUrls: addUrls,
+		addData: addData,
+		urls: getUrls
 	};
 })();
 
@@ -65,6 +132,29 @@ var WordFox = (function() {
 					sendResponse({status: true, message: "Scraping just stopped."});
 				} else if (request.message == "status") {
 					sendResponse({status: window.WordFox.status});
+				}
+				break;
+
+			case "contentscript":
+				if (request.message == "status") {
+					if (window.WordFox.status._initTabId == sender.tab.id &&
+						window.WordFox.status._initTabUrl == sender.tab.url) {
+						sendResponse({valid: true, message: "search-page"});
+					} else if (window.WordFox.status._detailTabIds.indexOf(sender.tab.id) > -1 &&
+						window.WordFox._status[sender.tab.id] == sender.tab.url) {
+						sendResponse({valid: true, message: "product-page"});
+					} else {
+						sendResponse({valid: false});
+					}
+				} else if (request.message == "product-urls") {
+					window.WordFox.addUrls(request);
+					if (request.next_page_url) {
+						sendResponse({continue: true});
+					} else {
+						sendResponse({continue: false});
+					}
+				} else if (request.message == "product-detail") {
+					window.WordFox.addData(request.data, sender, sendResponse);
 				}
 				break;
 
--- assets/scripts/contentscript.js
+++ assets/scripts/contentscript.js
@@ -1,5 +1,106 @@
 'use strict';
 
-(function () {
-	console.log('\'Allo \'Allo! Content script');
+var ContentScript = (function() {
+	var _hostname = window.location.hostname,
+		_pathname = window.location.pathname,
+		_url = window.location.href,
+
+		scrapSearchPage = function() {
+			var $searchItems = $("div#atfResults li.s-result-item div.s-position-relative a.a-link-normal"),
+				$nextPageLink = $("div#bottomBar #pagnNextLink"),
+				urls = [],
+				nextUrl = null;;
+
+			for (var i = 0; i < $searchItems.length; i++) {
+				urls.push($searchItems[i].href);
+			}
+			if ($nextPageLink) {
+				nextUrl = $nextPageLink[0].href;
+			}
+
+			chrome.extension.sendMessage({
+				from: "contentscript",
+				message: "product-urls",
+				urls: urls,
+				next_page_url: nextUrl
+			}, function(response) {
+				if (response.continue && $nextPageLink) {
+					window.location.href = nextUrl;
+				} else {
+					console.log("The extension doesn't work for this page.");
+				}
+			});
+		},
+
+		scrapProductPage = function() {
+			var $pageTitle = $("title"),
+				$title = $("#productTitle"),
+				$keyword = $("meta[name='keywords']"),
+				$landingImage = $("#landingImage"),
+				$brand = $("a#brand"),
+				$price = $("span#priceblock_ourprice"),
+				$BSL = $("li#SalesRank ul.zg_hrsr span.zg_hrsr_rank");
+
+			var data = {
+				pageTitle: ($pageTitle || {}).text,
+				title: ($title || {}).text(),
+				keywords: ($keywords[0] || []).content,
+				image: ($landingImage[0] || {}).src,
+				brand: ($brand.find("img")[0] || {}).src,
+				price: ($price || {}).text(),
+				BSL: ($BSL[0] || {}).textContent,
+				bsl: ($BSL[1] || {}).textContent
+			};
+
+			chrome.extension.sendMessage({
+				from: "contentscript",
+				message: "product-detail",
+				data: data
+			}, function(response) {
+				//
+			});
+			console.log("This is product page.");
+		},
+
+		_process = {
+			"search-page": scrapSearchPage,
+			"product-page": scrapProductPage
+		},
+		_status = {},
+
+		init = function() {
+			chrome.extension.sendMessage({
+				from: "contentscript",
+				message: "status"
+			}, function(response) {
+				if (response.valid) {
+					process(response.message);
+				} else {
+					console.log("The extension doesn't work for this page.");
+				}
+			});
+		},
+
+		process = function(flag) {
+			if (flag) {
+				_process[flag]();
+			}
+		},
+
+		getSomething = function() {
+			return [];
+		};
+
+	return {
+		init: init,
+		get: getSomething,
+		scrapProductPage: scrapProductPage
+	};
 })();
+
+(function (window, jQuery) {
+	console.log('\'Allo \'Allo! Content script');
+	window.ContentScript = ContentScript;
+	window.ContentScript.init();
+	// window.ContentScript.scrapProductPage();
+})(window, $);
